61321e6a45f48f418b12793bd8bfab97873c31ef
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2021-11-07 21:29:08 +0600

    division added

--- Code.js
+++ Code.js
@@ -1,14 +1,14 @@
 function add(x, y) {
-    //doc added
     return x + y;
 }
 
 function substract(x, y) {
-    return x - y;
 }
 
 function multiply(x, y) {
+
 }
 
 function division(x, y) {
+    return x / y;
 }
